24c48a5b0192dcd375939c8fc8a55521d7eb5851
Seungjun Lim <seungjunlim@Adminui-MacBook-Pro.local>
2018-10-25 17:02:00 +0900

    Add GasStationController

---
+++ OilDreamTest/OilDreamTest/Controllers/GasStationTypeController.swift
@@ -0,0 +1,17 @@
+//
+//  GasStationTypeController.swift
+//  OilDreamTest
+//
+//  Created by Seungjun Lim on 25/10/2018.
+//  Copyright © 2018 dave. All rights reserved.
+//
+
+import UIKit
+
+class GasStationTypeController: UITableViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.backgroundColor = .red
+        
+    }
+}
